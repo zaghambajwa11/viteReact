98318744a6209e172946d1f787a6a88da656d7b1
Zagham Arshad <zaghambajwa57@gmail.com>
2023-08-19 17:12:38 -0600

    initial

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,13 +1,63 @@
+import { useState, useEffect } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const BREEDS = [""];
+
 const SearchParams = () => {
-    const location = "Seattle, WA";
+   
+    const [location, setLocation] = useState("");
+    const [animal, setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    
     return (
       <div className="search-params">
+        
         <form>
           <label htmlFor="location">
             Location
-            <input id="location" value={location} placeholder="Location" />
+            <input onChange = {e => setLocation(e.target.value)} id="location" value={location} placeholder="Location" />
           </label>
+            <label htmlFor="animal">
+            Animal
+            <select
+                id="animal"
+                value={animal}
+                onChange={e => {
+                     setAnimal(e.target.value);
+                     setBreed("");
+                    }
+                }
+                onBlur={e => setAnimal(e.target.value)}
+            >
+                <option />
+                {ANIMALS.map(animal => (
+                <option key={animal} value={animal}>
+                    {animal}
+                </option>
+                ))}
+            </select>
+            </label>
+
+            <label htmlFor="breed">
+            breed
+            <select
+                id="breed"
+                disabled={!BREEDS.length}
+                value={breed}
+                onChange={e => setBreed(e.target.value)}
+                onBlur={e => setBreed(e.target.value)}
+            >
+                <option />
+                {BREEDS.map(breed => (
+                <option key={breed} value={breed}>
+                    {breed}
+                </option>
+                ))}
+            </select>
+            </label>
           <button>Submit</button>
+
+
         </form>
       </div>
     );
